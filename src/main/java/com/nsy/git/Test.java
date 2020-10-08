02226ce019a51a4d766e8675b31639118ccf9c99
宁尚野 <737493095@qq.com>
2020-10-08 13:59:25 +0800

    修改Test aaaaaaaaaaa

--- src/main/java/com/nsy/git/Test.java
+++ src/main/java/com/nsy/git/Test.java
@@ -7,5 +7,6 @@ package com.nsy.git;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Git Project");
+        System.out.println("Hello World！！");
     }
 }
