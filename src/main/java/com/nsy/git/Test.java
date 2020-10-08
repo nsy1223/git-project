73d624cff52b7077f30b2a9c625734a543970f26
宁尚野 <737493095@qq.com>
2020-10-08 13:50:16 +0800

    创建工程git-test

---
+++ src/main/java/com/nsy/git/Test.java
@@ -0,0 +1,11 @@
+package com.nsy.git;
+
+/**
+ * Created by 宁尚野
+ * 2020/10/08 ~ 13:46
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Git Project");
+    }
+}
